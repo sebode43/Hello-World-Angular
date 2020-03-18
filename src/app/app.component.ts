fb23319a141b2a86d7b799a9027a24b0187b0c03
max-student <max-student@maxtrain.com>
2020-03-18 16:25:04 -0400

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+//use Component class out of the angular core library
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+//inside the () is a singl jSON object with three keys
 export class AppComponent {
-  title = 'hello-world-angular';
+  title = 'Angular 9';
+  name: string = "No name yet!";
+  hColor : string = "red";
+  isHidden: boolean = false;
+
+  setName(): void{
+    this.name = "Sarah";
+  }
+
+toggleColor(): void{
+  this.hColor = (this.hColor == "red")?"blue":"red";
+}
+
+flipHidden(): void{
+  this.isHidden = !this.isHidden;
+}
+
 }
